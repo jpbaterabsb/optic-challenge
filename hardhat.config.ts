22e43181a8a9d894f0bf6cfc8b4b342ec3f06e70
JP <joao.santos-ctr@vectorsolutions.com>
2023-03-01 17:30:10 -0300

    remove keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,8 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://goerli.infura.io/v3/07d6c283a1084fa6a2c870990d73f7f5`,
-      accounts: ['869d66d3ddb5ea3cfab803480146ba2c3c01731460ddd24e7154f01fabe3ef8e']
+      url: `https://goerli.infura.io/v3/%%INFURA_API_KEY%%`,
+      accounts: ['%%YOUR_PRIVATE_KEY%%']
     }
   }
 
